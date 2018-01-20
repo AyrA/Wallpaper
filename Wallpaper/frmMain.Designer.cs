a2edaafb805e29569c18f2f76d89802e75667a1d
Kevin Gut <kevin.gut@bitm.sg>
2018-01-20 03:32:01 +0100

    Implementing Form

---
+++ Wallpaper/frmMain.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Wallpaper
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnChangeWP = new System.Windows.Forms.Button();
+            this.nudTimeout = new System.Windows.Forms.NumericUpDown();
+            this.tbDirectory = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.pbCurrent = new System.Windows.Forms.PictureBox();
+            this.lblInterval = new System.Windows.Forms.Label();
+            this.FBD = new System.Windows.Forms.FolderBrowserDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.nudTimeout)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCurrent)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnChangeWP
+            // 
+            this.btnChangeWP.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnChangeWP.Location = new System.Drawing.Point(237, 38);
+            this.btnChangeWP.Name = "btnChangeWP";
+            this.btnChangeWP.Size = new System.Drawing.Size(135, 23);
+            this.btnChangeWP.TabIndex = 4;
+            this.btnChangeWP.Text = "Change Wallpaper now";
+            this.btnChangeWP.UseVisualStyleBackColor = true;
+            this.btnChangeWP.Click += new System.EventHandler(this.btnChangeWP_Click);
+            // 
+            // nudTimeout
+            // 
+            this.nudTimeout.Location = new System.Drawing.Point(74, 41);
+            this.nudTimeout.Maximum = new decimal(new int[] {
+            86400,
+            0,
+            0,
+            0});
+            this.nudTimeout.Minimum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.nudTimeout.Name = "nudTimeout";
+            this.nudTimeout.Size = new System.Drawing.Size(70, 20);
+            this.nudTimeout.TabIndex = 3;
+            this.nudTimeout.Value = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            this.nudTimeout.ValueChanged += new System.EventHandler(this.nudTimeout_ValueChanged);
+            // 
+            // tbDirectory
+            // 
+            this.tbDirectory.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbDirectory.Location = new System.Drawing.Point(12, 12);
+            this.tbDirectory.Name = "tbDirectory";
+            this.tbDirectory.ReadOnly = true;
+            this.tbDirectory.Size = new System.Drawing.Size(316, 20);
+            this.tbDirectory.TabIndex = 0;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBrowse.Location = new System.Drawing.Point(334, 10);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(38, 23);
+            this.btnBrowse.TabIndex = 1;
+            this.btnBrowse.Text = "...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // pbCurrent
+            // 
+            this.pbCurrent.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pbCurrent.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbCurrent.Location = new System.Drawing.Point(12, 67);
+            this.pbCurrent.Name = "pbCurrent";
+            this.pbCurrent.Size = new System.Drawing.Size(360, 183);
+            this.pbCurrent.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbCurrent.TabIndex = 4;
+            this.pbCurrent.TabStop = false;
+            // 
+            // lblInterval
+            // 
+            this.lblInterval.AutoSize = true;
+            this.lblInterval.Location = new System.Drawing.Point(12, 43);
+            this.lblInterval.Name = "lblInterval";
+            this.lblInterval.Size = new System.Drawing.Size(56, 13);
+            this.lblInterval.TabIndex = 2;
+            this.lblInterval.Text = "Interval (s)";
+            // 
+            // FBD
+            // 
+            this.FBD.Description = "Select image Directory";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 262);
+            this.Controls.Add(this.lblInterval);
+            this.Controls.Add(this.pbCurrent);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.tbDirectory);
+            this.Controls.Add(this.nudTimeout);
+            this.Controls.Add(this.btnChangeWP);
+            this.MinimumSize = new System.Drawing.Size(400, 300);
+            this.Name = "frmMain";
+            this.Text = "Wallpaper Changer";
+            ((System.ComponentModel.ISupportInitialize)(this.nudTimeout)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCurrent)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnChangeWP;
+        private System.Windows.Forms.NumericUpDown nudTimeout;
+        private System.Windows.Forms.TextBox tbDirectory;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.PictureBox pbCurrent;
+        private System.Windows.Forms.Label lblInterval;
+        private System.Windows.Forms.FolderBrowserDialog FBD;
+    }
+}
+
---
+++ Wallpaper/frmMain.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Wallpaper
+{
+    /// <summary>
+    /// Main Form
+    /// </summary>
+    public partial class frmMain : Form
+    {
+        /// <summary>
+        /// Supported Images
+        /// </summary>
+        public const string IMAGE_EXT = ".bmp|.png|.jpg|.jpeg|.gif";
+
+        /// <summary>
+        /// Serializable settings
+        /// </summary>
+        public struct Settings
+        {
+            public string DirectoryName;
+            public string[] FileNames;
+            public int Timeout;
+        }
+
+        private List<string> Images;
+        private string LastDir;
+        private Random R;
+        private Timer T;
+        private string SettingsFile;
+
+        /// <summary>
+        /// Initializes form and processes settings
+        /// </summary>
+        public frmMain()
+        {
+            InitializeComponent();
+            R = new Random();
+            T = new Timer();
+            //Default
+            T.Interval = 60000;
+
+            SettingsFile = Path.Combine(Application.StartupPath, "settings.xml");
+            var Data = ReadSettings();
+            if (Data.FileNames != null)
+            {
+                Images = new List<string>(Data.FileNames);
+            }
+            if (Data.Timeout >= nudTimeout.Minimum && Data.Timeout<=nudTimeout.Maximum)
+            {
+                T.Interval = Data.Timeout * 1000;
+                nudTimeout.Value = Data.Timeout;
+            }
+            if (!string.IsNullOrEmpty(Data.DirectoryName))
+            {
+                FBD.SelectedPath = tbDirectory.Text = LastDir = Data.DirectoryName;
+            }
+            T.Tick += delegate
+            {
+                T.Stop();
+                SetRandomImage();
+                T.Start();
+            };
+            SetRandomImage();
+            T.Start();
+        }
+
+        /// <summary>
+        /// Saves current settings
+        /// </summary>
+        private void SaveSettings()
+        {
+            SaveSettings(new Settings() {
+                DirectoryName = LastDir,
+                FileNames = Images?.ToArray(),
+                Timeout = T.Interval / 1000
+            });
+        }
+
+        /// <summary>
+        /// Saves specific settings
+        /// </summary>
+        /// <param name="S">Settings</param>
+        private void SaveSettings(Settings S)
+        {
+            try
+            {
+                File.WriteAllText(SettingsFile, S.Serialize());
+            }
+            catch (Exception ex)
+            {
+                ShowError("Unable to save settings.\r\n" + ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Restores last settings
+        /// </summary>
+        /// <returns>Settings</returns>
+        private Settings ReadSettings()
+        {
+            if (File.Exists(SettingsFile))
+            {
+                try
+                {
+                    return File.ReadAllText(SettingsFile).Deserialize<Settings>();
+                }
+                catch (Exception ex)
+                {
+                    ShowError("Problem reading settings file.\r\n" + ex.Message);
+                }
+            }
+            return default(Settings);
+        }
+
+        /// <summary>
+        /// Removes a random item from the image list and returns it
+        /// </summary>
+        /// <returns>Random item, null if list is empty or uninitialized</returns>
+        private string PopRandom()
+        {
+            if (Images == null || Images.Count == 0)
+            {
+                return null;
+            }
+            lock (Images)
+            {
+                var I = R.Next(Images.Count);
+                var S = Images[I];
+                Images.RemoveAt(I);
+                return Path.Combine(LastDir, S);
+            }
+        }
+
+        /// <summary>
+        /// Checks if the given file name is an image file
+        /// </summary>
+        /// <param name="FileName">File name with ot without path</param>
+        /// <returns>true, if image</returns>
+        /// <remarks>Does test by file extension only</remarks>
+        private bool IsImageFile(string FileName)
+        {
+            return IMAGE_EXT.Split('|').Any(m => FileName.ToLower().EndsWith(m));
+        }
+
+        /// <summary>
+        /// Gets all image file names from a folder
+        /// </summary>
+        /// <param name="Folder">Directory to read images from</param>
+        private void GetImages(string Folder)
+        {
+            try
+            {
+                Images = new List<string>(Directory.GetFiles(Folder).Where(m => IsImageFile(m)).Select(m => m.Substring(Folder.Length).Trim('/', '\\')));
+                LastDir = Folder;
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentException("Invalid Directory", ex);
+            }
+        }
+
+        /// <summary>
+        /// Sets a random image from the list as wallpaper
+        /// </summary>
+        private void SetRandomImage()
+        {
+            if (Images == null || Images.Count == 0)
+            {
+                try
+                {
+                    GetImages(LastDir);
+                }
+                catch (Exception ex)
+                {
+                    ShowError(ex);
+                }
+            }
+            string ImageFile = PopRandom();
+            if (!string.IsNullOrEmpty(ImageFile))
+            {
+                if (File.Exists(ImageFile))
+                {
+                    DesktopWallpaper.Set(ImageFile, DesktopWallpaper.Style.Stretch);
+                    Image I = Image.FromFile(ImageFile);
+                    if (pbCurrent.Image != null)
+                    {
+                        pbCurrent.Image.Dispose();
+                    }
+                    pbCurrent.Image = I;
+                }
+            }
+            SaveSettings();
+        }
+
+        /// <summary>
+        /// Shows an error message
+        /// </summary>
+        /// <param name="Message">Error message</param>
+        private void ShowError(string Message)
+        {
+            MessageBox.Show(Message, "Problem setting Wallpaper", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        /// <summary>
+        /// Shows an error message generated from an exception
+        /// </summary>
+        /// <param name="ex">Exception</param>
+        private void ShowError(Exception ex)
+        {
+            ShowError(ex.Message);
+        }
+
+        private void btnChangeWP_Click(object sender, EventArgs e)
+        {
+            T.Stop();
+            SetRandomImage();
+            T.Start();
+        }
+
+        private void nudTimeout_ValueChanged(object sender, EventArgs e)
+        {
+            T.Interval = (int)nudTimeout.Value * 1000;
+            SaveSettings();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            if (FBD.ShowDialog() == DialogResult.OK)
+            {
+                GetImages(tbDirectory.Text = FBD.SelectedPath);
+                SaveSettings();
+            }
+        }
+    }
+}
