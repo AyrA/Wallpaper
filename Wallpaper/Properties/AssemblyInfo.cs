a78c889c76a4db515b87bae04492a37a4bfd2ec0
Kevin Gut <kevin.gut@bitm.sg>
2018-01-20 03:31:49 +0100

    cleanup

--- Wallpaper/Properties/AssemblyInfo.cs
+++ Wallpaper/Properties/AssemblyInfo.cs
@@ -1,12 +1,11 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Wallpaper")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Changes Wallpaper to a randomly selected image")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("AyrA")]
 [assembly: AssemblyProduct("Wallpaper")]
